47ee2c77916d5cf4333b92b52abd9404018a4bbb
JbCoder <JbCoder@users.noreply.github.com>
2020-05-26 16:18:30 +0100

    Update LROverlay.js

--- LROverlay.js
+++ LROverlay.js
@@ -467,7 +467,7 @@ function createLine(idno,x1,y1,x2,y2){
     var x2=x2*2
     var y2=y2*2
     var type = 2
-    var newLine={"x1":parseInt(x1),"y1":parseInt(y1),"x2":parseInt(x2),"y2":parseInt(y2),type}
+    var newLine={"x1":x1,"y1":y1,"x2":x2,"y2":y2,type}
     //if (y1 != 0 && y2 != data_height*2){
     	window.trackData.push(newLine)
 	//}
@@ -483,14 +483,12 @@ function floydSteinberg(sb, w, h)   // source buffer, width, height
          var cc = sb[ci];              // current color
          var rc = (cc<128?0:255);      // real (rounded) color
          var err = cc-rc;              // error amount
-         sb[ci] = rc;
-         ///*                  // saving real color
+         sb[ci] = rc;                // saving real color
          if(j+1<w) sb[ci  +1] += (err*7)>>4;  // if right neighbour exists
          if(i+1==h) continue;   // if we are in the last line
          if(j  >0) sb[ci+w-1] += (err*3)>>4;  // bottom left neighbour
                    sb[ci+w  ] += (err*5)>>4;  // bottom neighbour
          if(j+1<w) sb[ci+w+1] += (err*1)>>4;  // bottom right neighbour
-      	//*/
       }
 
     return sb
